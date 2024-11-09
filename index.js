15b41f79f717f07e1fd02dad85f873d9be085a12
anggita <anggitamaharani071@gmail.com>
2024-11-09 16:10:22 +0700

    Fetch API dan Visualisasi Data di React Native

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './Bottomnav';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
